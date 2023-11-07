5fabe775a0d46c4e0a0403d1550ec82cc9be9469
Prakul H N <112687428+prakulhn@users.noreply.github.com>
2023-11-07 12:17:54 +0530

    Create words_file.py

---
+++ words_file.py
@@ -0,0 +1,5 @@
+word_list = ["Jenny", "Apple", "Orange", "Brother", "Sister", "Care", "Innocent", "Child", "Snake", "Icecream",
+             "Board", "Shampoo", "Computer", "Laptop", "Umbrella", "Tablet", "Gadget", "Cake", "Bakery", "Table",
+             "Python", "Aeroplane", "Games", "Hangman", "Online", "Offline", "Hybrid", "Remote", "Onboard", "Onsite",
+             "Attitude", "Video", "Audio", "Word", "Work", "Free", "Rent", "Punit", "Evidence", "Kargil", "Vijay",
+             "Date", "Day", "Week", "Month", "Year", "Decade", "Century", "Weak", "Strong"]
